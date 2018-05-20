ef3e8056ba6eae17a0075fb837f474ea16dd887d
Henri Koskinen <hkoskinen@protonmail.com>
2018-05-20 18:17:51 +0300

    Build more styles

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import './search-bar.css';
 
 class SearchBar extends Component {
+  state = {
+    value: ''
+  }
+
+  onChange = (e) => {
+    this.setState({value: e.target.value});
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.value) return;
+
+    this.setState({value: ''});
+  }
+
   render() {
     return (
       <div className="search-bar">
-        <form>
-          <input type="text" className="search-bar__input" />
+        <form onSubmit={this.onSubmit}>
+          <div className="search-bar__content">
+            <input type="text" className="search-bar__input"
+              onChange={this.onChange} value={this.state.value} />
+            <button type="submit" className="search-bar__button">Search</button>
+          </div>
         </form>
       </div>
     );
